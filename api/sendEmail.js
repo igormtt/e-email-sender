e931e33441e27b54aa9b5f1ce49203abf8313fe1
Igor <igorg6149@gmail.com>
2025-02-08 17:36:27 -0300

    Utilizando Express

--- api/sendEmail.js
+++ api/sendEmail.js
@@ -41,16 +41,83 @@ app.post('/sendEmail', async (req, res) => {
   try {
     await transporter.sendMail({
       from: `Portfólio <${process.env.USER_MAIL}>`,
-      to: email,
+      to: process.env.USER_MAIL,
+      cc: email,
       subject: `Olá, sou ${nome}`,
-      text: message,
+      html: `
+      <!DOCTYPE html>
+      <html lang="pt-br">
+      <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Mensagem de Contato</title>
+        <style>
+          body {
+            font-family: Arial, sans-serif;
+            color: #333;
+            line-height: 1.6;
+          }
+          .header {
+            font-size: 24px;
+            color: #4CAF50;
+            font-weight: bold;
+          }
+          .message-content {
+            margin-top: 20px;
+          }
+          .message-content p {
+            font-size: 16px;
+            margin-bottom: 10px;
+          }
+          .message-content ul {
+            margin: 10px 0;
+          }
+          .message-content li {
+            margin-bottom: 8px;
+          }
+          .footer {
+            margin-top: 40px;
+            font-size: 14px;
+            color: #555;
+          }
+          .footer a {
+            color: #4CAF50;
+            text-decoration: none;
+          }
+        </style>
+      </head>
+      <body>
+        <div class="message-content">
+          <p class="header">Olá, sou o Igor!</p>
+      
+          <p>Esta é uma cópia para você!. Informações solicitadas:</p>
+      
+          <ul>
+            <li><strong>Nome:</strong> ${nome}</li>
+            <li><strong>E-mail:</strong> ${email}</li>
+            <li><strong>Mensagem:</strong></li>
+            <li><blockquote>${message}</blockquote></li>
+          </ul>
+      
+          <div class="footer">
+            <p>Caso queira discutir mais detalhes, entre em contato comigo através do meu e-mail: <a href="mailto:${email}">${email}</a>.</p>
+            <p>Atenciosamente,<br><strong>${nome}</strong></p>
+          </div>
+        </div>
+      </body>
+      </html>
+      `,
     });
-
+  
     return res.status(200).json({ status: 'E-mail enviado com sucesso!' });
   } catch (err) {
     console.error('Erro ao enviar o e-mail:', err);
     return res.status(500).json({ status: 'Houve um erro ao enviar o e-mail. Tente novamente.' });
   }
 });
+
+app.listen(8080, () => {
+  console.log('Rodando')
+})
 
 export default serverless(app);
